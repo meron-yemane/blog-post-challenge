c9a7478238c6a155fe8b1def2a306fd9b82f41c5
meron93 <meronyemane18@gmail.com>
2017-04-26 18:39:22 -0700

    travis check

--- tests/test-server.js
+++ tests/test-server.js
@@ -2,7 +2,6 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 
 const {app, runServer, closeServer} = require('../server');
-
 const should = chai.should();
 
 chai.use(chaiHttp);
